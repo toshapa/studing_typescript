b4fb8f314842aaa31a68031aa89b7c8e5b329db7
AP <tonichok@gmail.com>
2024-04-26 12:19:55 +0300

    long time refresh

--- test.js
+++ test.js
@@ -1,47 +1,54 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var pathDataJsonLocal = './data/data.json';
-var skillsArray = ['DoP', 'Cameraman', 'Juny Dev'];
-var infoData = {
-    officeId: 45,
-    isOpened: true,
-    contacts: {
-        phone: '+3232342342',
-        email: 'hgf@jmdfjdlk.com',
-        address: {
-            city: 'Kyiv',
-        },
-    },
-};
-// fs.readFile(pathDataJsonLocal, 'utf-8')
-//     .then((data) => JSON.parse(data))
-//     .then((resp) => console.log(resp))
-//     .catch((err) => console.log(err))
-for (var _i = 0, skillsArray_1 = skillsArray; _i < skillsArray_1.length; _i++) {
-    var skill = skillsArray_1[_i];
-    console.log("Is yuor skills ".concat(skill));
-}
+var node_fs_1 = require("node:fs");
+var pathDataJsonLocal = "./data.json";
+var skillsArray = ["DoP", "Cameraman", "Juny Dev"];
+// async function infoData(): Promise<USER> {
+//     const rawData = await fetch(pathDataJsonLocal, {
+//         method: "GET",
+//     })
+//     const processedData = await rawData.json()
+//     console.log(processedData)
+//     return processedData
+// }
+// infoData()
+// : Promise<{
+//     officeId: number
+//     isOpened: boolean
+//     contacts: {
+//         phone: string
+//         email: string
+//         address: { city: string }
+//     }
+// }, []>
+node_fs_1.promises.readFile(pathDataJsonLocal, "utf-8")
+    .then(function (data) { return JSON.parse(data); })
+    .then(function (resp) { return console.log(resp); })
+    .catch(function (err) { return console.log(err); });
+// for (let skill of skillsArray) {
+//     console.log(`Is yuor skills ${skill}`)
+// }
 var newSkillsArray = skillsArray
-    .filter(function (skill) { return skill !== 'DoP'; })
+    .filter(function (skill) { return skill !== "DoP"; })
     .map(function (skill) { return skill + " shit "; })
     .reduce(function (a, b) { return a + b; });
 var newbeSkilldepends = [
     123,
-    'DoP',
-    'Cameraman',
-    'Fron-End Develeper ',
+    "DoP",
+    "Cameraman",
+    "Fron-End Develeper ",
 ];
 var id = newbeSkilldepends[0];
-var cinemaSkills = newbeSkilldepends.filter(function (skill) { return typeof skill !== 'number'; });
-// console.log(cinemaSkills)
-var StatusCode;
-(function (StatusCode) {
-    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
-    StatusCode["FAILED"] = "Sun";
-    StatusCode["IN_PROCESS"] = "Night";
-})(StatusCode || (StatusCode = {}));
+var cinemaSkills = newbeSkilldepends.filter(function (skill) { return typeof skill !== "number"; });
 var respFromBank = {
-    message: 'Все добре',
-    statusCode: StatusCode.SUCCESS,
+    message: "Все добре",
+    statusCode: 1 /* StatusCode.SUCCESS */,
 };
-console.log(respFromBank);
+// console.log(respFromBank)
+// enum checkFetchAuth {
+//     GET = 'get',
+//     POST = 'post',
+// }
+// function fetchId(url: string, method: 'GET' | 'POST') {
+//     console.log()
+// }
--- test.ts
+++ test.ts
@@ -1,64 +1,89 @@
-import { promises as fs } from 'node:fs'
+import { promises as fs } from "node:fs"
 
-const pathDataJsonLocal = './data/data.json'
+const pathDataJsonLocal = "./data.json"
 
-let skillsArray: string[] = ['DoP', 'Cameraman', 'Juny Dev']
+let skillsArray: string[] = ["DoP", "Cameraman", "Juny Dev"]
 
-let infoData: {
+type PATH = string
+
+type USER = {
     officeId: number
     isOpened: boolean
     contacts: {
         phone: string
         email: string
-        address: { city: string }
-    }
-} = {
-    officeId: 45,
-    isOpened: true,
-    contacts: {
-        phone: '+3232342342',
-        email: 'hgf@jmdfjdlk.com',
         address: {
-            city: 'Kyiv',
-        },
-    },
+            city: string
+            address: string
+        }
+    }
 }
 
-// fs.readFile(pathDataJsonLocal, 'utf-8')
-//     .then((data) => JSON.parse(data))
-//     .then((resp) => console.log(resp))
-//     .catch((err) => console.log(err))
+// async function infoData(): Promise<USER> {
+//     const rawData = await fetch(pathDataJsonLocal, {
+//         method: "GET",
+//     })
+//     const processedData = await rawData.json()
+//     console.log(processedData)
+//     return processedData
+// }
 
-for (let skill of skillsArray) {
-    console.log(`Is yuor skills ${skill}`)
-}
+// infoData()
+
+// : Promise<{
+//     officeId: number
+//     isOpened: boolean
+//     contacts: {
+//         phone: string
+//         email: string
+//         address: { city: string }
+//     }
+// }, []>
+
+fs.readFile(pathDataJsonLocal, "utf-8")
+    .then((data) => JSON.parse(data))
+    .then((resp) => console.log(resp))
+    .catch((err) => console.log(err))
+
+// for (let skill of skillsArray) {
+//     console.log(`Is yuor skills ${skill}`)
+// }
 
 let newSkillsArray: string = skillsArray
-    .filter((skill: string) => skill !== 'DoP')
+    .filter((skill: string) => skill !== "DoP")
     .map((skill) => skill + ` shit `)
     .reduce((a, b) => a + b)
 
 const newbeSkilldepends: readonly [number, string, string, string] = [
     123,
-    'DoP',
-    'Cameraman',
-    'Fron-End Develeper ',
+    "DoP",
+    "Cameraman",
+    "Fron-End Develeper ",
 ]
 
 const id = newbeSkilldepends[0]
 const cinemaSkills = newbeSkilldepends.filter(
-    (skill) => typeof skill !== 'number'
+    (skill) => typeof skill !== "number"
 )
 // console.log(cinemaSkills)
 
-enum StatusCode {
+const enum StatusCode {
     SUCCESS = 1,
-    FAILED = 'Sun',
-    IN_PROCESS = 'Night',
+    FAILED = "Sun",
+    IN_PROCESS = "Night",
 }
 const respFromBank = {
-    message: 'Все добре',
+    message: "Все добре",
     statusCode: StatusCode.SUCCESS,
 }
 
-console.log(respFromBank)
+// console.log(respFromBank)
+
+// enum checkFetchAuth {
+//     GET = 'get',
+//     POST = 'post',
+// }
+
+// function fetchId(url: string, method: 'GET' | 'POST') {
+//     console.log()
+// }
