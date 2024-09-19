a442c727c2508c7565d483c0566d525878a20309
AP <tonichok@gmail.com>
2024-09-19 16:40:41 +0300

    generics

--- src/genericTS.ts
+++ src/genericTS.ts
@@ -4,3 +4,25 @@ function logMiddleware<T>(data: T): T {
 }
 
 const res = logMiddleware<number>(12)
+
+function getHalf<T>(data: Array<T>): Array<T> {
+    const t = data.length / 2
+    return data.splice(-2, t)
+}
+
+console.log(getHalf<number>([1, 2, 3, 4, 5, 4, 33]))
+
+const split: <T>(data: Array<T>) => Array<T> = getHalf
+
+interface ILogLine<T> {
+    date: Date
+    data: T
+}
+
+const logLine: ILogLine<{ b: number; c: number }> = {
+    date: new Date(),
+    data: {
+        b: 1,
+        c: 3,
+    },
+}
